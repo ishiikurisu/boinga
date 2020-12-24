d2c0e342df916bf69ab61830fef4d5b9e13c35a0
cris jr <crisjr@pm.me>
2020-12-24 19:10:16 -0300

    Translate to Go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ishiikurisu/boinga
+
+go 1.15
---
+++ main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/ishiikurisu/boinga"
+
+func main() {
+
+}
+func ToBoinga(inlet string) string {
+    return boinga.ToBoinga(inlet)
+}
+
+func FromBoinga(inlet string) string {
+    return boinga.FromBoinga(inlet)
+}
